5bfd90e9a794de857b48c05fbe2e76d16d81d647
Bluezzz <1447863932@qq.com>
2022-01-23 07:44:24 +0000

    删除文件 log.h

--- log.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOG__
-#define __LOG__
-
-int DebugLog(const char* format, ...);
-int DebugLog(const wchar_t* format, ...);
-int DebugError(const char* format, ...);
-int DebugError(const wchar_t* format, ...);
-
-#endif
